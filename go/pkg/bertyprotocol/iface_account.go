3bfaa556d9a8a19be88c28dd874480ac347e2211
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2020-03-19 18:33:19 +0100

    feat: bridge methods implementation

---
+++ go/pkg/bertyprotocol/iface_account.go
@@ -0,0 +1,57 @@
+package bertyprotocol
+
+import (
+	"crypto/rand"
+	"math"
+	"math/big"
+
+	"github.com/libp2p/go-libp2p-core/crypto"
+
+	"berty.tech/berty/go/pkg/errcode"
+)
+
+type AccountKeys interface {
+	AccountPrivKey() (crypto.PrivKey, error)
+	AccountProofPrivKey() (crypto.PrivKey, error)
+	DevicePrivKey() (crypto.PrivKey, error)
+	ContactGroupPrivKey(pk crypto.PubKey) (crypto.PrivKey, error)
+	MemberDeviceForGroup(g *Group) (*OwnMemberDevice, error)
+}
+
+// OwnMemberDevice is own local device part of a group
+type OwnMemberDevice struct {
+	Member crypto.PrivKey
+	Device crypto.PrivKey
+}
+
+func (d *OwnMemberDevice) Public() *MemberDevice {
+	return &MemberDevice{
+		Member: d.Member.GetPublic(),
+		Device: d.Device.GetPublic(),
+	}
+}
+
+// MemberDevice is a remote device part of a group
+type MemberDevice struct {
+	Member crypto.PubKey
+	Device crypto.PubKey
+	Secret *DeviceSecret
+}
+
+func NewDeviceSecret() (*DeviceSecret, error) {
+	counter, err := rand.Int(rand.Reader, big.NewInt(0).SetUint64(math.MaxUint64))
+	if err != nil {
+		return nil, errcode.ErrRandomGenerationFailed.Wrap(err)
+	}
+
+	chainKey := make([]byte, 32)
+	_, err = rand.Read(chainKey)
+	if err != nil {
+		return nil, errcode.ErrRandomGenerationFailed.Wrap(err)
+	}
+
+	return &DeviceSecret{
+		ChainKey: chainKey,
+		Counter:  counter.Uint64(),
+	}, nil
+}
