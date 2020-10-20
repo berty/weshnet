acc874babb6bab9bc7a7fa834847a2846413c377
D4ryl00 <d4ryl00@gmail.com>
2020-10-20 17:59:43 +0200

    chore: refactor the proximity transport and the MC driver
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

---
+++ go/internal/multipeer-connectivity-driver/mc-driver.h
@@ -0,0 +1,18 @@
+//
+//  mc-driver.h
+//  driver
+//
+//  Created by Rémi BARBERO on 30/03/2020.
+//  Copyright © 2020 Rémi BARBERO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void StartMCDriver(char *localPId);
+void StopMCDriver(void);
+int SendToPeer(char *remotePID, void *payload, int length);
+int DialPeer(char *remotePID);
+void CloseConnWithPeer(char *remotePID);
+int BridgeHandleFoundPeer(NSString *remotePID);
+void BridgeHandleLostPeer(NSString *remotePID);
+void BridgeReceiveFromPeer(NSString *remotePID, NSData *payload);
