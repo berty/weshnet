fc1767b1bd9439446eceee463bbfaa223525cfbc
D4ryl00 <d4ryl00@gmail.com>
2021-03-01 15:27:31 +0100

    feat: move cache from ios BLE driver to the proximity transport
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

---
+++ go/internal/proximitytransport/nativedriver.go
@@ -0,0 +1,57 @@
+package proximitytransport
+
+type NativeDriver interface {
+	// Start the native driver
+	Start(localPID string)
+
+	// Stop the native driver
+	Stop()
+
+	// Check if the native driver is connected to the remote peer
+	DialPeer(remotePID string) bool
+
+	// Send data to the remote peer
+	SendToPeer(remotePID string, payload []byte) bool
+
+	// Close the connection with the remote peer
+	CloseConnWithPeer(remotePID string)
+
+	// Return the multiaddress protocol code
+	ProtocolCode() int
+
+	// Return the multiaddress protocol name
+	ProtocolName() string
+
+	// Return the default multiaddress
+	DefaultAddr() string
+}
+
+type NoopNativeDriver struct {
+	protocolCode int
+	protocolName string
+	defaultAddr  string
+}
+
+func NewNoopNativeDriver(protocolCode int, protocolName, defaultAddr string) *NoopNativeDriver {
+	return &NoopNativeDriver{
+		protocolCode: protocolCode,
+		protocolName: protocolName,
+		defaultAddr:  defaultAddr,
+	}
+}
+
+func (d *NoopNativeDriver) Start(_ string) {}
+
+func (d *NoopNativeDriver) Stop() {}
+
+func (d *NoopNativeDriver) DialPeer(_ string) bool { return false }
+
+func (d *NoopNativeDriver) SendToPeer(_ string, _ []byte) bool { return false }
+
+func (d *NoopNativeDriver) CloseConnWithPeer(_ string) {}
+
+func (d *NoopNativeDriver) ProtocolCode() int { return d.protocolCode }
+
+func (d *NoopNativeDriver) ProtocolName() string { return d.protocolName }
+
+func (d *NoopNativeDriver) DefaultAddr() string { return d.defaultAddr }
