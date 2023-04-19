7a545738deaefe1346349564cf0c0fcbd0b50b91
Jeff Thompson <jeff@thefirst.org>
2023-04-19 16:18:27 +0200

    chore: Add doc comment for NewPersistentServiceClient .
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- service_client.go
+++ service_client.go
@@ -57,6 +57,13 @@ func NewInMemoryServiceClient() (ServiceClient, error) {
 	return NewServiceClient(opts)
 }
 
+// NewPersistentServiceClient creates a Wesh protocol service using persistent storage files in the
+// directory given by the directory path. If the directory doesn't exist, this creates it with files
+// of a new Wesh account and peer identity. (If the directory doesn't exist, this will create it only
+// if the parent directory exists. Otherwise you must first create the parent directories.) However,
+// if the persistent storage files already exist, then this opens them to use the existing Wesh
+// account and peer identity. This returns a gRPC ServiceClient which uses a direct in-memory
+// connection. When finished, you must call Close().
 func NewPersistentServiceClient(path string) (ServiceClient, error) {
 	var opts Opts
 
