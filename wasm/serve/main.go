a97781c847744e417ada125a6f5288f3d54ab67c
Norman Meier <norman@samourai.coop>
2024-06-10 16:58:12 +0200

    chore: add makefile
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- wasm/serve/main.go
+++ wasm/serve/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	fmt.Println("serving on :4242")
 	http.ListenAndServe(`:4242`, http.FileServer(http.Dir(`.`)))
 }
